aabf52ebe64193ece81effd0d4db6969311b55d1
Rahul Shah <rahul.shah4513@gmail.com>
2021-07-11 02:31:07 +0530

    Create Zip with Dictionaries.py

---
+++ Zip with Dictionaries.py
@@ -0,0 +1,12 @@
+# Zip lists: zipped_lists
+zipped_lists = zip(feature_names, row_vals)
+
+# Create a dictionary: rs_dict
+rs_dict = dict(zipped_lists)
+
+# Print the dictionary
+print(rs_dict)
+
+'''
+{'CountryName': 'Arab World', 'CountryCode': 'ARB', 'IndicatorName': 'Adolescent fertility rate (births per 1,000 women ages 15-19)', 'IndicatorCode': 'SP.ADO.TFRT', 'Year': '1960', 'Value': '133.56090740552298'}
+'''
