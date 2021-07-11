a382409f192e97c35a7329222bdea7e3061f0431
Rahul Shah <rahul.shah4513@gmail.com>
2021-07-11 21:14:55 +0530

    Create Numpy Shape and Type.py

---
+++ Numpy Shape and Type.py
@@ -0,0 +1,16 @@
+# baseball is available as a regular list of lists
+
+# Import numpy package
+import numpy as np
+
+# Create a 2D numpy array from baseball: np_baseball
+np_baseball = np.array(baseball)
+
+# Print out the shape of np_baseball
+print(np_baseball.shape)
+print(type(np_baseball))
+
+'''
+(1015, 2)
+<class 'numpy.ndarray'>
+'''
